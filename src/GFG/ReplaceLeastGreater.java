a14b60008678ee53ac318a45703f6c61ea92f530
Ritesh Kumar <ritesh.kumar@kloudspot.com>
2022-10-17 19:25:09 +0530

    Some Other Solutions

--- src/GFG/ReplaceLeastGreater.java
+++ src/GFG/ReplaceLeastGreater.java
@@ -1,19 +1,27 @@
 package GFG;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ReplaceLeastGreater {
 	public static void main(String[] args) {
-
+		int[] arr = new int[]{8, 58, 71, 18, 31, 32, 63, 92, 43, 3, 91, 93, 25, 80, 28};
+		System.out.println(Objects.requireNonNull(findLeastGreater(arr.length, arr)).toString());
 	}
 
 	public static ArrayList<Integer> findLeastGreater(int n, int[] arr) {
-		for (int i = 0; i < n; i++) {
-//			int pos = leastGreater(System.arraycopy(arr, i, ne, ););
-		}
-		return null;
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) list.add(leastGreater(arr, arr[i], i + 1));
+		return list;
 	}
-	static int leastGreater(int[] arr){
-		return 0;
+
+	static int leastGreater(int[] arr, int s, int start) {
+		int min = Integer.MAX_VALUE;
+		for (int i = start; i < arr.length; i++) if (arr[i] > s) if (arr[i] < min) min = arr[i];
+		if (min == Integer.MAX_VALUE) return -1;
+		return min;
 	}
 }
+
+// 18, 63, 80, 25, 32, 43, 80, 93, 80, 25, 93, -1, 28, -1, -1
+// 18, 63, 80, 25, 32, 43, 80, 93, 80, 25, 93, 2147483647, 28, 2147483647
