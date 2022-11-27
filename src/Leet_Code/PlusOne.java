99a0527d2767cfebaf12c8ca46aa93a7f8020e28
Ritesh__Kumar <ritesh.kumar@kloudspot.com>
2022-11-27 22:59:08 +0530

    Some new codes

--- src/Leet_Code/PlusOne.java
+++ src/Leet_Code/PlusOne.java
@@ -1,11 +1,14 @@
 package Leet_Code;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PlusOne {
 	public static void main(String[] args) {
 		int[] arr = {1, 3, 5, 0};
 		System.out.println(Arrays.toString(plusOne(arr)));
+		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(9, 6, 9));
+		System.out.println(increment(list, list.size()));
 	}
 
 	public static int[] plusOne(int[] digits) {
@@ -20,5 +23,20 @@ public class PlusOne {
 			return rst;
 		}
 		return digits;
+	}
+
+	static ArrayList<Integer> increment(ArrayList<Integer> arr, int N) {
+		if (N == 1) return new ArrayList<>(Arrays.asList(1, 0));
+		for (int i = N - 1; i >= 0; i--)
+			if (arr.get(i) != 9) {
+				arr.set(i, arr.get(i) + 1);
+				break;
+			} else arr.set(i, 0);
+		if (arr.get(0) == 0) {
+			for (int i = 0; i < N; i++) arr.set(i, 0);
+			arr.set(0, 1);
+			return arr;
+		}
+		return arr;
 	}
 }
---
+++ src/Leet_Code/Test.java
@@ -0,0 +1,14 @@
+package Leet_Code;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Stream;
+public class Test {
+	public static void main(String[] args) {
+//		Optional<String> s = Optional
+//		System.out.println(s);
+//
+
+	}
+}
